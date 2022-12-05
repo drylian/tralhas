32b78f3f549ad3b4dd306c812fe282eaf85a35b8
Drylian <109999325+drylian@users.noreply.github.com>
2022-12-05 03:21:08 -0300

    Create tela.sh

---
+++ tela.sh
@@ -0,0 +1,13 @@
+echo "Executar comando como Root caso de erro"
+
+echo "Olá bem-vindo ao sistema de telas , teste."
+
+echo "Executando apt install"
+
+sudo apt install -y xorg gdm3 gnome-backgrounds gnome-session adwaita-icon-theme-full gnome-themes-standard gnome-control-center gnome-tweaks software-properties-gtk network-manager pulseaudio gnome-terminal nautilus --no-install-recommends
+
+echo "Comandos executados com sucesso."
+
+echo "Reiniciando sistema para abrir tela"
+
+sudo reboot
