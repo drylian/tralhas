3dc747aafe2df7be42f54d9502fa12077244b268
Drylian <109999325+drylian@users.noreply.github.com>
2022-12-05 03:31:31 -0400

    A

--- tela.sh
+++ tela.sh
@@ -6,8 +6,11 @@ echo "Executando apt install"
 
 sudo apt install -y xorg gdm3 gnome-backgrounds gnome-session adwaita-icon-theme-full gnome-themes-standard gnome-control-center gnome-tweaks software-properties-gtk network-manager pulseaudio gnome-terminal nautilus --no-install-recommends
 
-echo "Comandos executados com sucesso."
+echo "Comandos de tela instalados com sucesso."
 
+echo "Instalando navegador Microsoft Edge"
+
+sudo dpkg -i package_file.deb
 echo "Reiniciando sistema para abrir tela"
 
 sudo reboot
