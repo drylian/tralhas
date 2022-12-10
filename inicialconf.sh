eded34aad7e7fbfe4decfa86477a40697a5ff771
Drylian <109999325+drylian@users.noreply.github.com>
2022-12-10 19:25:12 -0400

    Create inicialconf.sh

---
+++ inicialconf.sh
@@ -0,0 +1,7 @@
+echo "iniciando instalador"
+sudo apt install  wine
+sudo apt install wine-mono
+sudo apt install qjoypad
+sudo snap install discord 
+sudo apt install libfuse2
+sudo apt install zenity
