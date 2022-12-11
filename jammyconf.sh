831d7c5c2cae2b13b499f00da773c3c54fc76458
Drylian <109999325+drylian@users.noreply.github.com>
2022-12-11 08:59:58 -0400

    Create jammyconf.sh

---
+++ jammyconf.sh
@@ -0,0 +1,22 @@
+echo "iniciando instalador Versão Jammy(22.04.1+) , root necessario"
+sudo su
+echo "Instalando repos iniciais e outros."
+sudo apt apdate -y
+sudo apt upgrade -y
+echo "Instalando repo WineHQ"
+sudo dpkg --add-architecture i386 
+sudo mkdir -pm755 /etc/apt/keyrings
+sudo wget -O /etc/apt/keyrings/winehq-archive.key https://dl.winehq.org/wine-builds/winehq.key
+sudo wget -NP /etc/apt/sources.list.d/ https://dl.winehq.org/wine-builds/ubuntu/dists/jammy/winehq-jammy.sources
+echo "Atualizando Repo WineHQ"
+sudo apt update
+echo "Iniciando instalação do WineHQ Stable"
+sudo apt install --install-recommends winehq-stable
+echo "Instalação WineHQ completa"
+echo "Instalando Qjoypad(controles xbox),libfuse2 e zenity(para rodar appimage)"
+sudo apt install qjoypad -y
+sudo apt install libfuse2 -y
+sudo apt install zenity -y
+echo "Iniciando instalação do Gnome-disk-utility(Montador de disco)"
+sudo apt-get install gnome-disk-utility -y
+echo "instalação completa , por enquanto"
