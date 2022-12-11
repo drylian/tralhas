f6186b4f75936f5461b77d69a8f009e84ed9d4ab
Drylian <109999325+drylian@users.noreply.github.com>
2022-12-11 08:44:13 -0400

    Update and rename inicialconf.sh to Kineticconf.sh

---
+++ Kineticconf.sh
@@ -0,0 +1,23 @@
+echo "iniciando instalador Versão Kinetic Kudu(22.10) , root necessario"
+sudo su
+echo "Instalando repos iniciais e outros."
+sudo apt apdate -y
+sudo apt upgrade -y
+echo "Instalando repo WineHQ"
+sudo dpkg --add-architecture i386 
+sudo mkdir -pm755 /etc/apt/keyrings
+sudo wget -O /etc/apt/keyrings/winehq-archive.key https://dl.winehq.org/wine-builds/winehq.key
+sudo wget -NP /etc/apt/sources.list.d/ https://dl.winehq.org/wine-builds/ubuntu/dists/kinetic/winehq-kinetic.sources
+echo "Atualizando Repo WineHQ"
+sudo apt update
+echo "Iniciando instalação do WineHQ Stable"
+sudo apt install --install-recommends winehq-stable
+echo "Instalação WineHQ completa"
+echo "Instalando Qjoypad(controles xbox),discord,libfuse2 e zenity(para rodar appimage)"
+sudo apt install qjoypad -y
+sudo snap install discord -y
+sudo apt install libfuse2 -y
+sudo apt install zenity -y
+echo "Iniciando instalação do Gnome-disk-utility(Montador de disco)"
+sudo apt-get install gnome-disk-utility -y
+echo "instalação completa , por enquanto"
--- inicialconf.sh
+++
@@ -1,7 +0,0 @@
-echo "iniciando instalador"
-sudo apt install  wine
-sudo apt install wine-mono
-sudo apt install qjoypad
-sudo snap install discord 
-sudo apt install libfuse2
-sudo apt install zenity
