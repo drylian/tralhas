3d34099a32c0dd0236bf656bed045411a8a361f1
Drylian <109999325+drylian@users.noreply.github.com>
2023-01-17 22:25:20 -0400

    Update samp.sh

--- samp.sh
+++ samp.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
-    if [[ -f "./samp03svr" ]]; then
-        echo "==> O Samp Linux foi detectado, O Sistema de donwload não será necessario. <=="
-        echo "==> Setando permissões padrões. <=="
-        chmod 777 samp03svr
-        echo "==> Iniciando Servidor. <=="
-        ./samp03svr
-        echo "==> Servidor Iniciado. <=="
-    else
-        echo "==> O Samp Linux Não Detectado, O Sistema de donwload será iniciado. <=="
-        curl -L -o /home/container/samp03svr "https://github.com/drylian/tralhas/releases/latest/download/samp03svr"
-        echo "==> Download Terminado, iniciando configurações padrões. <=="
-        echo "==> Setando permissões padrões. <=="
-        chmod 777 samp03svr
-        echo "==> Iniciando Servidor. <=="
-        ./samp03svr
-        echo "==> Servidor Iniciado. <=="
+bold=$(echo -en "\e[1m")
+nc=$(echo -en "\e[0m")
+lightblue=$(echo -en "\e[94m")
+lightgreen=$(echo -en "\e[92m")
+
+if [[ -f "./samp03svr" ]]; then
+   echo "${bold}${lightgreen}==> O Samp Linux foi detectado, O Sistema de donwload não será necessario. <=="
+   echo "${bold}${lightgreen}==> Setando permissões padrões. <=="
+   chmod 777 samp03svr
+   echo "${bold}${lightgreen}==> Iniciando Servidor. <=="
+   ./samp03svr
+    echo "${bold}${lightgreen}==> Servidor Iniciado. <=="
+else
+    echo "${bold}${lightgreen}==> O Samp Linux Não Detectado, O Sistema de donwload será iniciado. <=="
+    curl -L -o /home/container/samp03svr "https://github.com/drylian/tralhas/releases/latest/download/samp03svr"
+    echo "${bold}${lightgreen}==> Download Terminado, iniciando configurações padrões. <=="
+    echo "${bold}${lightgreen}==> Setando permissões padrões. <=="
+    chmod 777 samp03svr
+    echo "${bold}${lightgreen}==> Iniciando Servidor. <=="
+    ./samp03svr
+    echo "${bold}${lightgreen}==> Servidor Iniciado. <=="
 fi
-echo "Completado"
+echo "${bold}${lightgreen}==>Terminado com Erro.<=="
