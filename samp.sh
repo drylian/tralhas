104590efe5000452208d6322582c648cfc90a6ad
Drylian <109999325+drylian@users.noreply.github.com>
2023-01-17 22:27:18 -0400

    Update samp.sh

--- samp.sh
+++ samp.sh
@@ -5,14 +5,14 @@ lightblue=$(echo -en "\e[94m")
 lightgreen=$(echo -en "\e[92m")
 
 if [[ -f "./samp03svr" ]]; then
-   echo "${bold}${lightgreen}==> O Samp Linux foi detectado, O Sistema de donwload não será necessario. <=="
+   echo "${bold}${lightgreen}==> O Samp Linux foi detectado, O Sistema de download não será necessario. <=="
    echo "${bold}${lightgreen}==> Setando permissões padrões. <=="
    chmod 777 samp03svr
    echo "${bold}${lightgreen}==> Iniciando Servidor. <=="
    ./samp03svr
     echo "${bold}${lightgreen}==> Servidor Iniciado. <=="
 else
-    echo "${bold}${lightgreen}==> O Samp Linux Não Detectado, O Sistema de donwload será iniciado. <=="
+    echo "${bold}${lightgreen}==> O Samp Linux Não Detectado, O Sistema de download será iniciado. <=="
     curl -L -o /home/container/samp03svr "https://github.com/drylian/tralhas/releases/latest/download/samp03svr"
     echo "${bold}${lightgreen}==> Download Terminado, iniciando configurações padrões. <=="
     echo "${bold}${lightgreen}==> Setando permissões padrões. <=="
