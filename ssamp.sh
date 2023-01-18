2c50b1f260a0e0900ee06c3cfa42b097184d289a
Drylian <109999325+drylian@users.noreply.github.com>
2023-01-17 23:17:07 -0400

    Create ssamp.sh

---
+++ ssamp.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+ 
+server_start() {
+screen /home/sampsvr/startgtaserver.sh & # put in full path and name of startup script
+} 
+ 
+server_stop() {
+killall startgtaserver.sh # your startup script name
+killall samp03svr # need to put in path to killall if its not in $PATH
+} 
+ 
+server_restart() {
+server_stop
+sleep 1
+server_start
+}
+case "$1" in
+'start')
+server_start
+;;
+'stop')
+server_stop
+;;
+'restart')
+server_restart
+;;
+*)
+echo "use $0 start|stop|restart"
+esac
